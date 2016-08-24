b2b3ef2c4b3acd99907542023c1b0695973bf20b
Jesús Romero <jesusromero@imac-de-arturo.local>
2016-08-24 01:07:01 -0500

    primer programa

---
+++ del_0_al_100.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for numeros in 0 ... 100{
+    
+    if numeros % 2 == 0 {
+        
+        print ("\(numeros) es par")
+    }else {
+        
+        print ("\t\t\t \(numeros) es impar")
+    }
+    if numeros % 5 == 0 && numeros != 0 {
+        
+        print("\t\t\t\t\t\t\t\(numeros) Bingo!!!")
+    }
+    
+    if numeros >= 30 && numeros <= 40 {
+        
+        print ("\t\t\t\t\t\t\t\t\t\t\t\(numeros) Viva Swift")
+        
+    }
+    
+}
